e4c23f757e52534c334cb5e3a0c5df29dfe3f711
sm-sayna-mobina <153964823+sm-sayna-mobina@users.noreply.github.com>
2023-12-18 01:23:50 +0100

    Add files via upload
    Upload code

---
+++ arp_project/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gcc src/master.c -lm -lrt  -o  bin/master
+gcc src/watch_dog.c -lm -lrt  -o  bin/watch_dog
+gcc src/drone.c -lm -lrt  -o  bin/drone
+gcc src/server.c -lm -lrt  -o  bin/server
+gcc src/window.c -lncurses -lm -lrt  -o  bin/window
+
+./bin/master
